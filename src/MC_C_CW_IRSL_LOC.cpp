03f3a3bdab43f76173528f74cac010ea65e04c70
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-02-25 17:01:08 -0500

    Add MC_C_CW_IRSL_LOC.cpp

---
+++ src/MC_C_CW_IRSL_LOC.cpp
@@ -0,0 +1,56 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Title:   MC_C_CW_IRSL_LOC.cpp (IRSL localised transiation)
+// Author:  Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), based on
+// code by Johannes Friedrich and equations provided by Vasilis Pagonis
+// Contact: sebastian.kreutzer@u-bordeaux-montaigne.fr
+// Date:    Sun Feb 24 14:59:39 2019
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// [[Rcpp::export("MC_C_CW_IRSL_LOC")]]
+List MC_C_CW_IRSL_LOC(arma::vec times, int n_filled, double r, double A) {
+  //n >> n_filled: concentration of filled traps [cm^-3]
+  //t >> times: refers basically to the temperature
+  //r: detrapping ratio [cm^-3]
+  //A: detrapping parameter [s^-1]
+
+  // set output matrices
+  NumericMatrix signal (times.size(), 1);
+  NumericMatrix remaining_e (times.size(), 1);
+  NumericVector r_num;
+
+    //this is p(t) ... here to keeo the code easy and consistent
+    double P = A;
+
+    //t-loop, means run over time
+    for(std::size_t t = 0; t < times.size(); ++t){
+
+          //n_filled; decide whether and electron will be excitated
+          for(int j = 0; j < n_filled; ++j){
+
+            //draw random number
+            r_num = runif(1);
+
+            if (r_num[0] < P)
+              n_filled = n_filled - 1;
+
+            if (n_filled == 0)
+              break;
+
+          } // end n_filled
+
+          //calculate signal and remaining filled
+          signal(t,0) = P * (pow(n_filled, 2) / (r + n_filled));
+          remaining_e(t,0) = n_filled;
+
+          if (n_filled == 0)
+            break;
+
+        } // end t-loop
+
+    return(Rcpp::List::create(Named("signal") = signal,
+                              Named("remaining_e") = remaining_e));
+}
