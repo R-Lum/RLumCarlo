09a7db810e680e2f3e1d7ac38503295f55c02ea9
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-07 19:54:19 -0400

    Modify package imports for NAMESPACE

--- R/RLumCarlo-package.R
+++ R/RLumCarlo-package.R
@@ -1,16 +1,13 @@
-#' Modelling luminescence signals in feldspar
+#' @aliases Monte-Carlo Methods for Simulating Luminescence Phenomena
 #'
 #' \if{html}{
 #' \figure{Logo_RLumCarlo.png}{options: width="50px" alt="https://github.com/R-Lum/RLumCarlo"}\cr
 #' }
 #'
 #'
-#' \tabular{ll}{ Package: \tab RLumCarlo\cr Type: \tab Package\cr Version:
-#' \tab 0.0.2\cr Date: \tab 2018-08-28 \cr License: \tab GPL-3\cr }
-#'
 #' @name RLumCarlo-package
 #' @docType package
-#' @author Johannes Friedrich (University of Bayreuth, Germany), Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS-Université Bordeaux Montaigne (France)
+#' @author Johannes Friedrich (University of Bayreuth, Germany), Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS-Université Bordeaux Montaigne (France), Vasilis Pagonis, McDaniel College Westminster (MD, USA), Christoph Schmidt, University of Bayreuth (Germany)
 #'
 #' @importFrom grDevices adjustcolor
 #' @import magrittr
@@ -19,9 +16,7 @@
 #' @import parallel
 #' @import doParallel
 #' @import methods
-#' @importFrom graphics lines
-#' @importFrom graphics plot
-#' @importFrom graphics polygon
+#' @importFrom graphics plot lines polygon grid par legend
 #' @importFrom Rcpp evalCpp
 #' @importFrom utils modifyList
 #' @useDynLib RLumCarlo, .registration = TRUE
