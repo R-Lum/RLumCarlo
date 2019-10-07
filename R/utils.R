b1876ce66c9b78a1d95ef1f6c0ed7d97146de320
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-07 16:36:32 -0400

    Add internal function to create model output list

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,21 @@
-#' @noMd
+#' Combine Model Ouput Array
+#'
+#' @noRd
 comb_array <- function(...) abind::abind(..., along = 3)
+
+#' Create RLumCarlo Model Output List
+#'
+#' @md
+#' @noRd
+.return_ModelOutput <- function(signal, time, model = sys.call(which = -1)){
+  temp <- list(signal = signal, temp = temp)
+  attributes(temp) <- list(
+    class = "RLumCarlo_Model_Output",
+    model = if(!is.null(model)) model else NA_character_
+
+  )
+
+  return(temp)
+}
+
+
