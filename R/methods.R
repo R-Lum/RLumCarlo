dff19f79435892bc60bb2db60c128416b0839b77
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-07 22:30:02 -0400

    calc_RLumCarlo() becomes summary method

--- R/methods.R
+++ R/methods.R
@@ -1,7 +1,11 @@
-#' Plot results from Monte-Carlo simulations with RLumCarlo
+#' @title Summarize RLumCarlo Modelling Results
 #'
-#' @param results [list] of class RLumCarlo_Model_Output: RLumCarlo simulation output object
+#' @description Summarize RLumCarlo Modelling results, so that they can be plotted easily
+#'
+#' @param object [list] of class RLumCarlo_Model_Output: RLumCarlo simulation output object
 #' produced by all
+#'
+#' @param ... further arguments passed to the method
 #'
 #' @return This function returns a [data.frame]
 #'
@@ -9,21 +13,17 @@
 #'
 #' @author Johannes Friedrich, University of Bayreuth (Germany), Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS-Université Bordeaux Montagine, France
 #'
+#' @method summary RLumCarlo_Model_Output
 #' @md
 #' @export
-calc_RLumCarlo <- function(results){
-
-  # Pre-checks ----------------------------------------------------------------------------------
-  if(class(results) != "RLumCarlo_Model_Output")
-    stop("[calc_RLumCarlo()] Input must be of class 'RLumModel_Model_Output' created by functions starting with 'run_'!",
-         call. = FALSE)
+summary.RLumCarlo_Model_Output <- function(object, ...){
 
   # copy input into new objects
-  signal <- results[[1]]
-  times <- results[[2]]
+  signal <- object[[1]]
+  times <- object[[2]]
 
   # melt objects
-  if(length(dim(results)) == 2) {
+  if(length(dim(object)) == 2) {
       sum_signal <- vapply(1:length(times), function(x){
         sum(signal[x,])
       }, FUN.VALUE = 1)
@@ -50,7 +50,7 @@ calc_RLumCarlo <- function(results){
 
   ## set output data.frame
   output <- data.frame(time = times, avg = avg, y_min = y_min, y_max = y_max)
-  attr(output, "model") <- attributes(results)$model
+  attr(output, "model") <- attributes(object)$model
 
   ## return
   return(output)
