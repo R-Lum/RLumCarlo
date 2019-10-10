e63736a9d648f21db4b1c7b1244e6dd624536c83
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-10 16:54:38 -0400

    Update tests for summary()

---
+++ tests/testthat/test_methods.R
@@ -0,0 +1,18 @@
+context("methods")
+
+test_that("basic run", {
+  testthat::skip_on_cran()
+
+  ## summary
+  expect_is(summary(run_MC_TL_TUN(
+    s = 3.5e12,
+    E = 1.45,
+    rho = 0.015,
+    r_c = 0.5,
+    times = 100:500,
+    method = "seq"
+  )), "data.frame")
+
+
+})
+
