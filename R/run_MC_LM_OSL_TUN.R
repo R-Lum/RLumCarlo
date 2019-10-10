e38c9e394bb80131e351fb45d7feadcac5a55af2
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-10 12:30:30 -0400

    Updated reference for run_MC_LM_OSL_TUN.R

--- R/run_MC_LM_OSL_TUN.R
+++ R/run_MC_LM_OSL_TUN.R
@@ -33,9 +33,7 @@
 #'
 #' @author Johannes Friedrich, University of Bayreuth (Germany)
 #'
-#' @references
-#'
-#' Pagonis, V. and Kulp, C. (2017) ‘Monte Carlo simulations of tunneling phenomena and nearest neighbor hopping mechanism in feldspars’, Journal of Luminescence. Elsevier, 181, pp. 114–120. doi: 10.1016/j.jlumin.2016.09.014.
+#' @references Pagonis, V., Friedrich, J., Discher, M., Müller-Kirschbaum, A., Schlosser, V., Kreutzer, S., Chen, R. and Schmidt, C., 2019. Excited state luminescence signals from a random distribution of defects: A new Monte Carlo simulation approach for feldspar. Journal of Luminescence 207, 266–272. \doi{10.1016/j.jlumin.2018.11.024}
 #'
 #' @examples
 #'
