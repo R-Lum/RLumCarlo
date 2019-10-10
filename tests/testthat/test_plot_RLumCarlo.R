d6b684c7dedb6ebb3c9306da1d137a72059341f1
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-10-10 15:12:05 -0400

    Add test for plot_RLumCarlo()

---
+++ tests/testthat/test_plot_RLumCarlo.R
@@ -0,0 +1,28 @@
+context("plot_RLumCarlo")
+
+test_that("basic run", {
+  testthat::skip_on_cran()
+
+  ## create data to plot
+  results_par <- lapply(c(1.2, 1.4), function(r){
+    run_MC_TL_TUN(
+    s = 3.5e12,
+    E = 1.45,
+    rho = 0.015,
+    r_c = r,
+    times = 100:500,
+    method = "par"
+  )})
+
+  ## break plot function
+  expect_error(plot_RLumCarlo(object = "error"), "'object' needs to be of class RLumCarlo_Model_Output!")
+  expect_error(plot_RLumCarlo(object = list("error")), "At least one element in the list is not of class RLumCarlo_Model_Output!")
+
+  ## simple run, one dataset
+  expect_silent(plot_RLumCarlo(object = results_par[[1]]))
+
+  ## plot the list
+  expect_silent(plot_RLumCarlo(object = results_par, main = "Test 2"))
+
+})
+
