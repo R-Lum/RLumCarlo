b274829d643846994d75e72eddf88a07a5327584
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-11-01 14:07:43 +0100

    Documentation utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,26 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Internal, non-exported package functions
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #' Combine Model Ouput Array
+#'
+#' @param ... Arrrays to be combined
+#'
+#' @author Johannes Friedrich, University of Bayreuth (Germany)
 #'
 #' @noRd
 comb_array <- function(...) abind::abind(..., along = 3)
 
+
 #' Create RLumCarlo Model Output List
+#'
+#' @param signal [numeric] (**required**): signal vector
+#'
+#' @param time [numeric] (**required**): time vector
+#'
+#' @param model [character] (*with default*): the name of the model, the functions tries
+#' to set this automatically.
+#'
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS-Université Bordeaux-Montaigne (France)
 #'
 #' @md
 #' @noRd
