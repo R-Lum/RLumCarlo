9ed3f3b20a079f5dca8f2137873e44af982d57fb
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-04-28 13:35:55 +0200

    Add internal util.h and util.cpp as helper functions; also add relevant Makevars

---
+++ inst/include/util.h
@@ -0,0 +1,11 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Title:   util.h
+// Purpose: Header for internal C++ helper functions
+// Author:  Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom),
+// Contact: sebastian.kreutzer@aber.ac.uk
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#ifndef CALC_DELTAT_H
+#define CALC_DELTAT_H
+#include <RcppArmadillo.h>
+  double calc_deltat(arma::vec times);
+#endif
