766fc6107421e48375e5730c358d403f53e70c4b
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-04-28 21:58:44 +0200

    Update test

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -3,8 +3,8 @@ context("utils")
 test_that("basic run", {
   testthat::skip_on_cran()
 
-  ## .registerClusters
-  cl <- expect_silent(.registerClusters(method = "par", cores = NULL, FALSE))
+  ## .registerClusters ... cores and verbose
+  cl <- expect_type(.registerClusters(method = "par", cores = NULL, verbose = TRUE), "list")
   parallel::stopCluster(cl)
 
 
