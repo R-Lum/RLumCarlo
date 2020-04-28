2186973be64dff1f38c5fef9a8ed0048aeefe37d
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-04-28 19:13:00 +0200

    Update test to make it more useful

--- tests/testthat/test_run_MC_LM_OSL_TUN.R
+++ tests/testthat/test_run_MC_LM_OSL_TUN.R
@@ -2,39 +2,39 @@ context("run_MC_LM_OSL_TUN")
 
 test_that("basic run", {
   testthat::skip_on_cran()
-  
+
   ##break the function on purpose
   expect_error(run_MC_LM_OSL_TUN(method = "error"), "Allowed keywords for 'method' are either 'par' or 'seq'!")
   expect_error(run_MC_LM_OSL_TUN(output = "error"), "Allowed keywords for 'output' are either 'signal' or 'remaining_e'!")
-  
+
   ## run function with basic setting
   ## sequential and multicore
   results_seq <- expect_silent(run_MC_LM_OSL_TUN(
     A = 1,
-    rho = 1e-7,
-    times = 0:100,
+    rho = 1e-2,
+    times = 0:1000,
     clusters = 15,
     N_e = 2,
-    r_c = 0.001,
+    r_c = 0.01,
     delta.r = 1e-1,
     method = "seq"
   ))
-  
+
   results_par <- expect_silent(run_MC_LM_OSL_TUN(
     A = 1,
-    rho = 1e-7,
+    rho = 1e-2,
     times = 0:100,
     clusters = 15,
     N_e = 2,
-    r_c = 0.001,
+    r_c = 0.01,
     delta.r = 1e-1,
     method = "par"
   ))
-  
+
   ## check output
   expect_s3_class(results_par, class = "RLumCarlo_Model_Output")
   expect_length(results_par, 2)
   expect_s3_class(results_seq, class = "RLumCarlo_Model_Output")
   expect_length(results_seq, 2)
-  
+
 })
