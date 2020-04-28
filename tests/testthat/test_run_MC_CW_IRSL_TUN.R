3255e5a31e111a4362f4b89777d8b8f55c441a30
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-04-28 19:53:49 +0200

    Update test ... it did not work as expected

--- tests/testthat/test_run_MC_CW_IRSL_TUN.R
+++ tests/testthat/test_run_MC_CW_IRSL_TUN.R
@@ -2,39 +2,39 @@ context("run_MC_CW_IRSL_TUN")
 
 test_that("basic run", {
   testthat::skip_on_cran()
-  
+
   ##break the function on purpose
   expect_error(run_MC_CW_IRSL_TUN(method = "error"), "Allowed keywords for 'method' are either 'par' or 'seq'!")
   expect_error(run_MC_CW_IRSL_TUN(output = "error"), "Allowed keywords for 'output' are either 'signal' or 'remaining_e'!")
-  
+
   ## run function with basic setting
-  ## sequential and multicore
+  ## sequential and multi-core
   results_seq <- expect_silent(run_MC_CW_IRSL_TUN(
-    A = 1e-3,
-    rho = 1e-7,
+    A = 0.8,
+    rho = 1e-4,
     times = 0:100,
-    clusters = 1,
+    clusters = 10,
     N_e = 2,
-    r_c = 1,
-    delta.r = 1e-3,
+    r_c = 0.05,
+    delta.r = 0.1,
     method = "seq"
   ))
-  
+
   results_par <- expect_silent(run_MC_CW_IRSL_TUN(
-    A = 1e-3,
-    rho = 1e-7,
+    A = 0.8,
+    rho = 1e-4,
     times = 0:100,
-    clusters = 1,
+    clusters = 10,
     N_e = 2,
-    r_c = 1,
-    delta.r = 1e-3,
+    r_c = 0.05,
+    delta.r = 0.1,
     method = "par"
   ))
-  
+
   ## check output
   expect_s3_class(results_par, class = "RLumCarlo_Model_Output")
   expect_length(results_par, 2)
   expect_s3_class(results_seq, class = "RLumCarlo_Model_Output")
   expect_length(results_seq, 2)
-  
+
 })
