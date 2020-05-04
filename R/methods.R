bf5c98d575ade287ec4911da63adcc179aa5bcf5
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-05-04 18:55:47 +0200

    Add new S3-method for concatenate c()

--- R/methods.R
+++ R/methods.R
@@ -1,6 +1,26 @@
 #' methods_RLumCarlo
 #'
+#' Methods for S3-generics implemented for the package 'RLumCarlo'.
+#'
 #' @keywords internal
+#'
+#' @examples
+#'
+#' ##create object
+#' object <- run_MC_TL_DELOC(
+#'  s = 3.5e12,
+#'  E = 1.45,
+#'  R = 0.1,
+#'  method = 'seq',
+#'  clusters = 100,
+#'  times = 150:350)
+#'
+#' ##summary
+#' summary(object)
+#'
+#' ##combine
+#' c(objects,objects)
+#'
 #' @md
 #' @name methods_RLumCarlo
 NULL
@@ -47,12 +67,12 @@ summary.RLumCarlo_Model_Output <- function(object, verbose = TRUE, ...){
       clusters <- dim(signal)[3]
 
       ##get sum signal
-      sum_signal <- sapply(1:clusters, function(y){
+      sum_signal <- vapply(1:clusters, function(y){
         vapply(1:length(times), function(x){
           sum(signal[x,,y])
         }, FUN.VALUE = 1)
 
-      })
+      }, numeric(length(times)))
 
       mean <- rowMeans(sum_signal)
       sd <- apply(sum_signal, 1, sd)
@@ -75,4 +95,57 @@ summary.RLumCarlo_Model_Output <- function(object, verbose = TRUE, ...){
 
   ## return
   invisible(output)
+}
+
+#' @title Combine modelling results
+#'
+#' @param ... objects to be concatenated
+#'
+#' @return This function returns a the same as the input objects
+#'
+#' @section Function version: 0.1.0
+#'
+#' @author Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom)
+#'
+#' @md
+#' @method c RLumCarlo_Model_Output
+#' @rdname methods_RLumCarlo
+#' @export
+c.RLumCarlo_Model_Output <- function(...){
+  ## remove everything that does not belong into this list
+  objects <- lapply(list(...), function(x) if(class(x) == "RLumCarlo_Model_Output") x)
+  objects[sapply(objects, is.null)] <- NULL
+
+  ## we can only merge results from the same stimulation mode
+  ## get modes
+  modes <- vapply(objects, attr, character(1), which = "model")
+  modes <- vapply(strsplit(modes, "_", fixed = TRUE), function(x) x[3], character(1))
+
+  ## get the one we remove
+  if(length(rm_objects <- which(modes[1] != modes)) != 0){
+    warning(paste0("Object(s) ", paste(rm_objects, collapse = ", "), " do not have the same stimulation mode as the first object and were removed from the input!"), call. = FALSE)
+    objects[rm_objects] <- NULL
+
+  }
+
+  ## make sure that the time vector is identically
+  if(!unique(vapply(objects, function(x) length(x$time), numeric(1))))
+    stop("[c.RLumCarlo_Model_Output()] You cannot combine objects with different time vectors!", call. = FALSE)
+
+  ## in order to combine, we have to treat values coming from the tunnelling
+  ## process differently ...
+  objects <- lapply(objects, function(x){
+    if(dim(x$signal)[2] > 1){
+      x$signal <- vapply(1:dim(x$signal)[3], function(o)
+        matrix(rowSums(x$signal[, , o]), ncol = 1),
+        matrix(numeric(nrow(x$signal[,,1])), ncol = 1))
+    }
+    return(x)
+  })
+
+  # finally combine the objects
+  .return_ModelOutput(
+    signal = comb_array(lapply(objects, function(x) x$signal)),
+    time = objects[[1]]$time,
+    model = attr(objects[[1]], which = "model"))
 }
