bf7650eacffdaaf24e57b81dfab091b8f7945f01
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-05-04 20:40:17 +0200

    Update tests to make sure they test everything

--- tests/testthat/test_run_MC_CW_IRSL_LOC.R
+++ tests/testthat/test_run_MC_CW_IRSL_LOC.R
@@ -2,35 +2,35 @@ context("run_MC_CW_IRSL_LOC")
 
 test_that("basic run", {
   testthat::skip_on_cran()
-  
+
   ##break the function on purpose
   expect_error(run_MC_CW_IRSL_LOC(method = "error"), "Allowed keywords for 'method' are either 'par' or 'seq'!")
   expect_error(run_MC_CW_IRSL_LOC(output = "error"), "Allowed keywords for 'output' are either 'signal' or 'remaining_e'!")
-  
+
   ## run function with basic setting
   ## sequential and multicore
   results_seq <- expect_silent(run_MC_CW_IRSL_LOC(
-    A = 1e-3,
+    A = 0.12,
     times = 0:100,
-    clusters = 1e1,
-    n_filled = 1,
+    clusters = 50,
+    n_filled = 100,
     r = 1e-7,
     method = "seq"
   ))
-  
+
   results_par <- expect_silent(run_MC_CW_IRSL_LOC(
-    A = 1e-3,
+    A = 0.12,
     times = 0:100,
-    clusters = 1e1,
-    n_filled = 1,
+    clusters = 50,
+    n_filled = 100,
     r = 1e-7,
     method = "par"
   ))
-  
+
   ## check output
   expect_s3_class(results_par, class = "RLumCarlo_Model_Output")
   expect_length(results_par, 2)
   expect_s3_class(results_seq, class = "RLumCarlo_Model_Output")
   expect_length(results_seq, 2)
-  
+
 })
